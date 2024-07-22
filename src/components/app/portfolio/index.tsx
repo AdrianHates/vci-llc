8ebaad67d1006c953bfa14aac70c79219fd39dc1
AizenSousuke <kurono1807@gmail.com>
2024-07-22 02:17:38 -0500

    added section portfolio

--- src/components/app/portfolio/index.tsx
+++ src/components/app/portfolio/index.tsx
@@ -1,23 +1,59 @@
+import React from "react";
+import cx from "../../libs/cx";
+
 const Portfolio = () => {
+  const listPortfolio: { text: string; class_img: string; link?: string }[] = [
+    {
+      link: "https://averliz.com/",
+      class_img: "w-[93px]",
+      text: "Somos una Neo Cooperativa especializada en atención al cliente con servicios financieros de primera clase; con la nueva vanguardia de seguridad, apegados a las leyes en créditos, depósitos a plazos, cuentas de ahorro y otros productos.",
+    },
+    {
+      class_img: "w-[185px]",
+      text: "Ofrecemos créditos a personas que normalmente no tienen oportunidad de inclusión financiera en la banca tradicional. Nuestros servicios son rápidos y seguros para nuestros clientes e inversionistas.",
+    },
+    {
+      class_img: "w-[92.55px]",
+      text: "Nos dedicamos al otorgamiento de microcréditos de corto plazo. Trabajando 24/7, con el uso de inteligencia artificial ofrecemos aprobaciones en minutos y otorgamiento de fondos instantáneos.",
+    },
+  ];
   return (
-    <section>
-      <div>
-        <h3>Portafolio de empresas</h3>
-        <p>
+    <section className="pb-[115px]">
+      <div className="bg-white py-[67px] w-[1146px] mx-auto text-[16px] leading-[19.5px] font-light gap-10 flex flex-col shadow-[0px_4px_4px_0_#24364B40] rounded-[24px] mt-[-160px]">
+        <h3 className="text-center text-[32px] leading-[39.01px] text-[#EE7623] font-bold">
+          Portafolio de empresas
+        </h3>
+        <p className="max-w-[65ch] mx-auto text-center">
           Conoce más sobre nuestro portafolio de empresas, que son un pilar
           fundamental en nuestro esfuerzo por asegurar la inclusión financiera.
         </p>
-        <div className="flex">
-          <div>
-            <img />
-            <p>
-              Somos una Neo Cooperativa especializada en atención al cliente con
-              servicios financieros de primera clase; con la nueva vanguardia de
-              seguridad, apegados a las leyes en créditos, depósitos a plazos,
-              cuentas de ahorro y otros productos.
-            </p>
+        {listPortfolio && (
+          <div className="flex items-center justify-center pt-8">
+            {listPortfolio.map((ulist, i) => (
+              <React.Fragment key={i}>
+                {i !== 0 && (
+                  <div className="bg-[#24364B1F] w-[1px] h-32 mx-7 mb-16"></div>
+                )}
+                <div className="flex flex-col items-center">
+                  <div className="h-[69px] flex items-center justify-center mb-8">
+                    <img
+                      src={`/portfolio/portfolio_img_${i + 1}.png`}
+                      className={cx("", ulist.class_img)}
+                    />
+                  </div>
+                  <p className="max-w-[25ch] h-[183px] text-center">
+                    {ulist.text}
+                  </p>
+                  <a href={ulist.link} target="_blank">
+                    <button className="bg-[#24364B] text-white text-[16px] leading-[19.5px] py-2.5 px-[22px] rounded-[8px] font-light">
+                      Visitar
+                    </button>
+                  </a>
+                </div>
+              </React.Fragment>
+            ))}
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
--- src/components/app/proyections/index.tsx
+++ src/components/app/proyections/index.tsx
@@ -1,6 +1,6 @@
 const Proyections = () => {
   return (
-    <section className="bg-quinary py-24">
+    <section className="bg-quinary pt-24 pb-40">
       <h3 className="mx-auto max-w-[15ch] text-center font-nanum-myeongjo text-tertiary font-semibold text-[64px] leading-[55px]">
         Verri Capital Investments, LLC
       </h3>
