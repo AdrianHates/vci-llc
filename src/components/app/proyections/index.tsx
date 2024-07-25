e040a2d60be909af09bd388748d6cac863c357e5
AizenSousuke <kurono1807@gmail.com>
2024-07-25 14:07:44 -0500

    responsive proyections section sm(640)

--- src/components/app/proyections/index.tsx
+++ src/components/app/proyections/index.tsx
@@ -3,11 +3,11 @@ interface Props {
 }
 const Proyections = ({ id }: Props) => {
   return (
-    <section className="bg-primary bg-opacity-[5%] pt-24 pb-40" id={id}>
-      <h3 className="mx-auto max-w-[15ch] text-center font-nanum-myeongjo text-tertiary font-semibold text-[64px] leading-[55px]">
+    <section className="bg-primary bg-opacity-[5%] px-5 pt-24 pb-40" id={id}>
+      <h3 className="mx-auto max-w-[15ch] text-center font-nanum-myeongjo text-tertiary font-semibold sm:text-[64px] text-[32px] sm:leading-[55px] leading-[30.94px]">
         Verri Capital Investments, LLC
       </h3>
-      <p className="text-[18px] leading-[21.94px] font-light max-w-[80ch] text-center mx-auto my-10">
+      <p className="sm:text-[18px] text-[16px] sm:leading-[21.94px] leading-[19.5px] font-light max-w-[80ch] text-center mx-auto my-10">
         <span className="font-semibold text-quaternary">
           Somos un conglomerado Americano
         </span>{" "}
